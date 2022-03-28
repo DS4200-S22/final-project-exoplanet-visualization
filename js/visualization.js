e6f6167fd1dfc30de761285b4af152772baecc72
nick-coury <nick.coury@sbcglobal.net>
2022-03-28 12:25:06 -0400

    Update visualization.js

--- js/visualization.js
+++ js/visualization.js
@@ -8,7 +8,7 @@ const svg1 = d3.select("#vis-container")
                 .attr("height", height - margin.top - margin.bottom)
                 .attr("viewBox", [0, 0, width, height])
                 .call( d3.brush() 
-                    .extent([[margin.left,margin.top], [width + margin.left - margin.right, height + margin.top - margin.bottom]])); 
+                    .extent([[margin.left, margin.top], [width + margin.left, height + margin.top ]])); 
 
 const svg2 = d3.select("#vis-container")
                 .append("svg")
