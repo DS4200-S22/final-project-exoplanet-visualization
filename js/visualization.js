283936f38844062fdf674721afe3432e208241e5
nick-coury <nick.coury@sbcglobal.net>
2022-04-06 23:03:38 -0400

    Update visualization.js

--- js/visualization.js
+++ js/visualization.js
@@ -262,7 +262,7 @@ d3.csv("data/cleanedExoplanetData.csv").then((data) => {
                  .attr("class", "bar") 
                  .attr("x", (d,i) => x3(i)) 
                  .attr("y", (d) => y3(d.dataPoints)) 
-                 .attr("height", (d) => height - margin.bottom - (y3(maxY3)))  
+                 .attr("height", (d) => height - margin.bottom - (y3(d.dataPoints)))  
                  .attr("width", x3.bandwidth())
                  .style("fill", "green")    
         // Bars
