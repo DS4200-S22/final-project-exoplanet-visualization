490b9b845dd1a59ecec56d62b40ba445c8c49ec1
nick-coury <nick.coury@sbcglobal.net>
2022-04-06 23:10:33 -0400

    Update visualization.js

--- js/visualization.js
+++ js/visualization.js
@@ -22,6 +22,12 @@ const svg3 = d3.select("#vis-container")
                 .attr("width", width - margin.left - margin.right)
                 .attr("height", height - margin.top - margin.bottom)
                 .attr("viewBox", [0, 0, width, height])
+
+const svg4 = d3.select("#vis-container")
+                .append("svg")
+                .attr("width", width - margin.left - margin.right)
+                .attr("height", height - margin.top - margin.bottom)
+                .attr("viewBox", [0, 0, width, height])
                     
 
 // Plotting 
@@ -191,8 +197,6 @@ d3.csv("data/cleanedExoplanetData.csv").then((data) => {
         // Finx max y 
         let maxY3 = 76;
 
-        //console.log("Max X3: ", maxX3);
-
         const thresholds = d3.range(maxX3 + 1)
         const binner = d3.bin().value(d=>d[xKey3]).thresholds(thresholds).domain([0,maxX3])
         const binned = binner(data)
@@ -255,7 +259,7 @@ d3.csv("data/cleanedExoplanetData.csv").then((data) => {
                                
         }
            
-        myBars = svg3.selectAll(".bar")
+        bars1 = svg3.selectAll(".bar")
                 .data(medians)
                 .enter()
                 .append("rect") 
@@ -264,17 +268,93 @@ d3.csv("data/cleanedExoplanetData.csv").then((data) => {
                  .attr("y", (d) => y3(d.dataPoints)) 
                  .attr("height", (d) => height - margin.bottom - (y3(d.dataPoints)))  
                  .attr("width", x3.bandwidth())
-                 .style("fill", "green")    
-        // Bars
-            // svg3.selectAll("mybar")
-            //     .data(medians[i])
-            //     .enter()
-            //     .append("rect")
-            //         .attr("x", function(d) { return x3(d.dataPoints); })
-            //         .attr("y", function(d) { return y3(d.medianMass); })
-            //         .attr("width", x3.bandwidth())
-            //         .attr("height", function(d) { return height - y3(maxY3); })
-            //         .attr("fill", "#69b3a2")
-        
+                 .style("fill", "green")      
+    }
+
+    // Plot 4 
+    {
+        xKey4 = "radius"
+
+        // Find max x
+        let maxX4 = d3.max(data, (d) => { return d[xKey4]; });
+
+        // Find max y 
+        let maxY4 = 76;
+
+        const thresholds = d3.range(maxX4 + 1)
+        const binner = d3.bin().value(d=>d[xKey4]).thresholds(thresholds).domain([0,maxX4])
+        const binned = binner(data)
+        const mediansRadius = binned.map(bin => {
+            return {
+                medianRadius: d3.median(bin, b=>b[xKey4]),
+                dataPoints: bin.length,
+                bucketMin: bin.x0,
+                bucketMax: bin.x1
+
+            }
+        })
+
+
+        console.log(mediansRadius)
+        console.log(mediansRadius[0].dataPoints)
+        console.log(mediansRadius.length)
+
+        /*
+        // Create y scale   
+        let y4 = d3.scaleLinear()
+            .domain([0,maxY4])
+            .range([height-margin.bottom,margin.top]); 
+
+        // Create x scale
+        let x4 = d3.scaleBand()
+            .domain(d3.range(mediansRadius.length))
+            .range([margin.left, width - margin.right])
+            .padding(0.1); 
+
+        // Add x axis 
+        svg4.append("g")
+            .attr("transform", `translate(0,${height - margin.bottom})`) 
+            .call(d3.axisBottom(x4))   
+            .attr("font-size", '20px')
+            .call((g) => g.append("text")
+                .attr("x", width - margin.right)
+                .attr("y", margin.bottom)
+                .attr("fill", "black")
+                .attr("text-anchor", "end")
+                .text(xKey4)); 
+
+        // Add y axis 
+        svg4.append("g")
+            .attr("transform", `translate(${margin.left}, 0)`) 
+            .call(d3.axisLeft(y4)) 
+            .attr("font-size", '20px') 
+            .call((g) => g.append("text")
+                .attr("x", 0)
+                .attr("y", margin.top)
+                .attr("fill", "black")
+                .attr("text-anchor", "end")
+                .text("frequency"));
+
+        for (i = 0; i < mediansRadius.length; i++){            
+            dataP = mediansRadius[i].dataPoints;
+            medianR = mediansRadius[i].medianRadius;
+            
+            console.log("medians[" + i + "]: ");
+            console.log("d.dataPoints: " + dataP);
+            console.log("d.medianMass: " + medianR);
+                               
+        }
+           
+        bars2 = svg4.selectAll(".bar")
+                .data(mediansRadius)
+                .enter()
+                .append("rect") 
+                 .attr("class", "bar") 
+                 .attr("x", (d,i) => x4(i)) 
+                 .attr("y", (d) => y4(d.dataPoints)) 
+                 .attr("height", (d) => height - margin.bottom - (y4(d.dataPoints)))  
+                 .attr("width", x4.bandwidth())
+                 .style("fill", "green") 
+        */         
     }
 })                
